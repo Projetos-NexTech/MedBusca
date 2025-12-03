7990114f466867659a2af73eba231f8cc2e1bc8c
Pedro Paixão <pedrohenriqueeekkj@gmail.com>
2025-12-03 11:01:15 -0300

    fix(api.js): trocando a URL dos endpoints

--- front/src/service/api.js
+++ front/src/service/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create ({
     // local: baseURL: "http://localhost:5000",
-    baseURL: "http://backend:5000",
+    baseURL: "http://backend-production-e39a.up.railway.app",
     withCredentials: true,
 });
 
