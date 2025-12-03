641a7a269e7832c65af06ad886a099887c93bc72
Pedro Paixão <pedrohenriqueeekkj@gmail.com>
2025-12-02 23:46:48 -0300

    Update baseURL for API configuration

--- front/src/service/api.js
+++ front/src/service/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create ({
-    baseURL: "http://localhost:5000",
+    // local: baseURL: "http://localhost:5000",
+    baseURL: "backend.railway.internal,
     withCrdentials: true,
 });
 
