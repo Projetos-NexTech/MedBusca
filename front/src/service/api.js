2c06bd78d1dc64da98f6fc613b2157fce2d616dd
Pedro Paixão <pedrohenriqueeekkj@gmail.com>
2025-12-03 10:47:50 -0300

    fix(api.js): arrumando erro de sintaxe

--- front/src/service/api.js
+++ front/src/service/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const api = axios.create ({
     // local: baseURL: "http://localhost:5000",
-    baseURL: "http://backend:5000,
-    withCrdentials: true,
+    baseURL: "http://backend:5000",
+    withCredentials: true,
 });
 
 export default api;
